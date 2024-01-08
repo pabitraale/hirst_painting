512b7b5ae88215306b7cc4a3fd98c5ff0ccdfa2c
pabitraale <pabitraale25@yahoo.com>
2024-01-08 16:44:18 -0500

    added comments and few line of codes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from turtle import Turtle, Screen
 import random
 
 turtle.colormode(255)
-list_of_colors = [(199, 12, 32),(158, 63, 20), (249, 236, 25), (209, 88, 225), (40, 77, 187), (239, 229, 4), (38, 217, 69), (227, 160, 50)]
+list_of_colors = [(199, 12, 32), (158, 63, 20), (249, 236, 25), (209, 88, 225), (40, 77, 187), (239, 229, 4), (38, 217, 69), (227, 160, 50)]
 # Create turtle
 t = Turtle()
 
@@ -12,6 +12,7 @@ t.penup()
 t.speed(20)
 t.setposition(-200, -200)
 t.hideturtle()
+
 # Total number of dots
 dots_number = 100
 for count in range(1, dots_number+ 1):
